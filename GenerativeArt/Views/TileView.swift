162185969af41a9302c40256f4fbf1112e562de1
David Li <dtli@lcs.on.ca>
2023-10-24 11:44:54 -0400

    Organize files and groups in the project

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  TileView.swift
 //  GenerativeArt
 //
 //  Created by David Li on 2023-10-24.
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TileView: View {
     var body: some View {
         VStack {
             Rectangle()
+                //Make the shape be a square
                 .aspectRatio(1.0, contentMode: .fit)
                 
         }
@@ -19,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    TileView()
 }
