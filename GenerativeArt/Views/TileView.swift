16187881ea32b52968ea4f4603051f2388b99ba0
David Li <dtli@lcs.on.ca>
2023-10-24 12:22:17 -0400

    Adding Helper group to organiza the code

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -7,69 +7,7 @@
 
 import SwiftUI
 
-struct TriangleTopRight: Shape{
-    func path(in rect: CGRect) -> Path {
-        //Make an empty path
-        var path = Path()
-        
-        //Define the path
-        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        
-        //return the path
-        return path
-    }
-    
-    
-}
 
-struct TriangleBottomLeft: Shape{
-    func path(in rect: CGRect) -> Path {
-        //Make an empty path
-        var path = Path()
-        
-        //Define the path
-        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        
-        //return the path
-        return path
-    }
-}
-
-struct TriangleTopLeft: Shape{
-    func path(in rect: CGRect) -> Path {
-        //Make an empty path
-        var path = Path()
-        
-        //Define the path
-        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        //return the path
-        return path
-    }
-}
-
-struct TriangleBottomRight: Shape{
-    func path(in rect: CGRect) -> Path {
-        //Make an empty path
-        var path = Path()
-        
-        //Define the path
-        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxY, y: rect.maxY))
-        //return the path
-        return path
-    }
-}
 struct TileView: View {
     var body: some View {
         VStack {
