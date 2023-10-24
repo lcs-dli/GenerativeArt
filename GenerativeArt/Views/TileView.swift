666620e6f2382da2b6b0c2252b9e0f1042fb0056
David Li <dtli@lcs.on.ca>
2023-10-24 12:26:46 -0400

    Created a random state to stimulate a coin and draw the form.

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -9,25 +9,32 @@ import SwiftUI
 
 
 struct TileView: View {
+    //MARK: Stored property
+    let flipOne = Bool.random()
+    
+    //MARK: Computing property
     var body: some View {
-        VStack {
-            TriangleTopRight()
-                //.fill(.black)
-                .stroke(.black)
-                .aspectRatio(1.0, contentMode: .fit)
-                
-            TriangleBottomLeft()
-                //.fill(.black)
-                .stroke(.black)
-                .aspectRatio(1.0, contentMode: .fit)
-            TriangleTopLeft()
-                //.fill(.black)
-                .stroke(.black)
-                .aspectRatio(1.0, contentMode: .fit)
-            TriangleBottomLeft()
-                //.fill(.black)
-                .stroke(.black)
-                .aspectRatio(1.0, contentMode: .fit)
+        ZStack {
+            if(flipOne){
+                TriangleTopRight()
+                    //.fill(.black)
+                    .stroke(.black)
+                    .aspectRatio(1.0, contentMode: .fit)
+                    
+                TriangleBottomLeft()
+                    //.fill(.black)
+                    .stroke(.black)
+                    .aspectRatio(1.0, contentMode: .fit)
+            }else{
+                TriangleTopLeft()
+                    //.fill(.black)
+                    .stroke(.black)
+                    .aspectRatio(1.0, contentMode: .fit)
+                TriangleBottomRight()
+                    //.fill(.black)
+                    .stroke(.black)
+                    .aspectRatio(1.0, contentMode: .fit)
+            }
         }
         .padding()
     }
