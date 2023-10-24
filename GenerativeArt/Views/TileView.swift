abbc030f0b11833e82cc897515cc096d4e2c796b
David Li <dtli@lcs.on.ca>
2023-10-24 11:55:57 -0400

    Drawing top-right triangle and Add model for other triangle

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -7,10 +7,63 @@
 
 import SwiftUI
 
+struct TriangleTopRight: Shape{
+    func path(in rect: CGRect) -> Path {
+        //Make an empty path
+        var path = Path()
+        
+        //Define the path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        //return the path
+        return path
+    }
+    
+    
+}
+
+struct TriangleBottomLeft: Shape{
+    func path(in rect: CGRect) -> Path {
+        //Make an empty path
+        var path = Path()
+        
+        //Define the path
+        
+        //return the path
+        return path
+    }
+}
+
+struct TriangleTopLeft: Shape{
+    func path(in rect: CGRect) -> Path {
+        //Make an empty path
+        var path = Path()
+        
+        //Define the path
+        
+        //return the path
+        return path
+    }
+}
+
+struct TriangleBottomRight: Shape{
+    func path(in rect: CGRect) -> Path {
+        //Make an empty path
+        var path = Path()
+        
+        //Define the path
+        
+        //return the path
+        return path
+    }
+}
 struct TileView: View {
     var body: some View {
         VStack {
-            Rectangle()
+            TriangleTopRight()
                 //Make the shape be a square
                 .aspectRatio(1.0, contentMode: .fit)
                 
