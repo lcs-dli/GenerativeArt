f0fc47cd72cda74b4cc4554aaf463543a1503eb6
David Li <dtli@lcs.on.ca>
2023-10-26 14:46:01 -0400

    Adding color to the program. Add the second flipper to iomplement color slelection.

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -13,12 +13,37 @@ struct TileView: View {
     //MARK: Stored property
     let flipOne = Coin.flip()
     
+    //choosing markers
+    let markerOne = Color.red
+    let marketTwo = Color.blue
+    
+    //Devide on colors
+    let flipForColor = Coin.flip()
+    
     //MARK: Computing property
+    
+    //Set the colors based on the coin flip
+    var color1: Color{
+        if(flipForColor == .heads){
+            return markerOne
+        }else{
+            return marketTwo
+        }
+    }
+    
+    var color2: Color{
+        if(flipForColor == .tails){
+            return markerOne
+        }else{
+            return marketTwo
+        }
+    }
+    
     var body: some View {
         ZStack {
             if(flipOne == .heads){
                 TriangleTopRight()
-                    //.fill(.black)
+                    .fill(color1)
                     .stroke(.black)
                     .aspectRatio(1.0, contentMode: .fit)
                     
@@ -28,11 +53,11 @@ struct TileView: View {
                     .aspectRatio(1.0, contentMode: .fit)
             }else{
                 TriangleTopLeft()
-                    //.fill(.black)
+                    .fill(color2)
                     .stroke(.black)
                     .aspectRatio(1.0, contentMode: .fit)
                 TriangleBottomRight()
-                    //.fill(.black)
+                    .fill(color1)
                     .stroke(.black)
                     .aspectRatio(1.0, contentMode: .fit)
             }
